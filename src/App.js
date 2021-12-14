47a5e45710b7ebfb47303339aa48d7352b55d24a
MikitaCat <nikita.proger21@gmail.com>
2021-12-14 14:26:56 +0300

    Basic components and markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import react from 'react';
+import React from 'react';
+import FetchedPosts from './components/FetchedPosts';
+import { PostForm } from './components/PostForm';
+import Posts from './components/Posts';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Work</h1>
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h2>Posts List:</h2>
+          <Posts />
+        </div>
+        <div className="col">
+          <h2>Async Posts List:</h2>
+          <FetchedPosts />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>PostForm</h1>
+      </div>
+    );
+  }
+}
